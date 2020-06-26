dce5ff2490cde9dde359c46503c3a0624c94fc9e
Tyler Alsop <tylerjalsop@gmail.com>
2020-06-26 17:53:38 -0600

    Commented out the old code and added Psuedo Code to try and plan out how to do this.

--- names/names.py
+++ names/names.py
@@ -13,10 +13,17 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+########## Pseudo Code ##########
+# Create a binary search tree class. It will need insert and contains functions.
+# Initialize a binary search tree with an empty string for the initial value.
+# Take all of the names in names_1 file and use the bst.insert() method to add them to the tree.
+# Use the bst.contains() methods to check the bst for names from names_2 file.
+    # If the bst already contains a name in names_2 file then add it to the "duplicates" list above using the append() method.
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
